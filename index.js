aa188a15495f73f9a3b94a6cedaee19c808c6015
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-05-16 15:13:36 +0200

    Done with Day 2 - Coding Exercises

---
+++ index.js
@@ -0,0 +1,21 @@
+// Declare two variables with strings. Your name and your surname.
+let name = "Abenathi";
+let lastName = "Sindapi";
+
+// Concat the two variables. (This is done by adding a + in between the two names) You should see the data of the two joined 
+// on the output.
+let concat = name +" "+ lastName;
+console.log(concat);
+
+// Declare another variable with the name numberVar and the value being 5.
+let numberVar = 5;
+
+//Type in the console numberVar * 5. What is the output?
+console.log(numberVar * 5);
+// The output is 25
+
+// Create a constant with the value as your name. Try to reassign the value with your surname. Did you get an error?
+const name2 = "Abenathi";
+const name2 = "Sindapi";
+console.log(name2)
+// I am getting errors when I reassign my value
